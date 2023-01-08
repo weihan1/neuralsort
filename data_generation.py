7644a85b380babf8753ac3997ee58637632d6a8e
weihan <weihan.luo@mail.utoronto.ca>
2023-01-07 21:38:10 -0500

    added data processing files

--- data_generation.py
+++ data_generation.py
@@ -2,23 +2,31 @@ import random as random
 import torch
 from utils import RANDOM_SEED
 import numpy as np
-def train_test_data_gen(size, number_of_lists, seed=RANDOM_SEED):
+from random import sample
+def train_test_data_gen(size, number_of_lists, percentage_of_train, seed=RANDOM_SEED):
     '''
     Generate number_of_lists times random lists of unique integers <= size of size 'size'
     for training and generate the same number of lists for testing.
 
-    Return a tuple (number_of_lists, size), (number_of_lists, size) containing the 
-    lists for training and testing.
+    Return a 4-tuple, X_train, X_test, y_train, y_test
+    
     '''
     random.seed(seed)
     lst = []
     sorted_lst = []
     for i in range(number_of_lists):
-        lst.append(random.sample(range(1, size+1), size))
+        lst.append(random.sample(range(1, 1000+1), size))
         sorted_lst.append(sorted(lst[i]))
-    
-    return torch.tensor(lst, dtype= float,requires_grad=True), \
-        torch.tensor(sorted_lst, dtype= float,requires_grad=True)
+    X_train = sample(lst, int(percentage_of_train * len(lst)))
+    X_test = [sublist for sublist in lst if sublist not in X_train]
+    y_train = [sorted(lst) for lst in X_train]
+    y_test = [sorted(lst) for lst in X_test]
+
+    return torch.tensor(X_train, dtype=float, requires_grad=True), \
+            torch.tensor(X_test, dtype=float, requires_grad=True), \
+            torch.tensor(y_train, dtype=float, requires_grad=True), \
+            torch.tensor(y_test, dtype=float, requires_grad=True) 
+
 
 
 
--- utils.py
+++ utils.py
@@ -3,5 +3,7 @@ Utiliy functions for the project. Contains constants.
 """
 
 import os
+import torch
 
 RANDOM_SEED = 69
+device = "cuda" if torch.cuda.is_available() else "cpu"
